4d6ad0fe81e185dd749ecf9b20358d37f92fbb68
Dan Richert <dan.richert@gmail.com>
2012-01-24 18:31:07 -0500

    Adding specs

--- spec/moby/parts_of_speech_spec.rb
+++ spec/moby/parts_of_speech_spec.rb
@@ -2,6 +2,23 @@ require 'spec_helper'
 
 module Moby
   describe PartsOfSpeech do
+    let(:pos) { PartsOfSpeech.new }
+
+    subject { pos }
     its(:pos) { should be_a(Hash) }
+
+    describe "@pos Hash" do
+      subject { pos.pos }
+
+      describe "A1c" do
+        it { should have_key("A1c") }
+        its(["A1c"]) { should == "N" }
+      end
+
+      describe "backbit" do
+        it { should have_key("backbit") }
+        its(["backbit"]) { should == "V" }
+      end
+    end
   end
 end
