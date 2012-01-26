a6d590fb2c567ac011f4ae88ec93f0430d1690c3
Dan Richert <dan.richert@gmail.com>
2012-01-25 21:01:42 -0500

    Add PartsOfSpeech#find

--- lib/moby/parts_of_speech.rb
+++ lib/moby/parts_of_speech.rb
@@ -4,6 +4,10 @@ module Moby
       load_pos_db
     end
 
+    def find(word)
+      { word: word, code: pos_code(word), pos: pos_breakdown(word) }
+    end
+
     private
       def load_pos_db
         path = %w{share moby parts-of-speech mobypos.UTF-8.txt}
@@ -16,23 +20,34 @@ module Moby
         ]
       end
 
-      def code_map
-        {
-          :N => :noun,
-          :p => :plural,
-          :h => :noun_phrase,
-          :V => :verb_usu_participle,
-          :i => :verb_intransitive,
-          :a => :adjective,
-          :v => :adverb,
-          :C => :conjunction,
-          :P => :preposition,
-          :! => :interjection,
-          :r => :pronoun,
-          :D => :definite_article,
-          :I => :indefinite_article,
-          :o => :nominative
+      # Get part of speech code for word
+      def pos_code(word)
+        @pos[word]
+      end
+
+      def pos_code_map
+        @pos_code_map ||= {
+          "N" => :noun,
+          "p" => :plural,
+          "h" => :noun_phrase,
+          "V" => :verb_usu_participle,
+          "t" => :verb_transitive,
+          "i" => :verb_intransitive,
+          "A" => :adjective,
+          "v" => :adverb,
+          "C" => :conjunction,
+          "P" => :preposition,
+          "!" => :interjection,
+          "r" => :pronoun,
+          "D" => :definite_article,
+          "I" => :indefinite_article,
+          "o" => :nominative
         }
+      end
+
+      # Convert POS code to array of descriptive symbols from #pos_code_map
+      def pos_breakdown(word)
+        pos_code(word).chars.to_a.map {|c| pos_code_map[c] }
       end
   end
 end
--- spec/moby/parts_of_speech_spec.rb
+++ spec/moby/parts_of_speech_spec.rb
@@ -18,5 +18,37 @@ module Moby
       its(["flam"])        { should == "NV" }
       its(["garnishee"])   { should == "NVt" }
     end
+
+    describe "#find" do
+      describe "rabbling" do
+        subject { pos.find("rabbling") }
+        its([:word]) { should == "rabbling" }
+        its([:code]) { should == "Vti" }
+        its([:pos]) {
+          should == [:verb_usu_participle, :verb_transitive, :verb_intransitive]
+        }
+      end
+
+      describe "saccharine" do
+        subject { pos.find("saccharine") }
+        its([:word]) { should == "saccharine" }
+        its([:code]) { should == "A" }
+        its([:pos])  { should == [:adjective] }
+      end
+
+      describe "tailgate" do
+        subject { pos.find("tailgate") }
+        its([:word]) { should == "tailgate" }
+        its([:code]) { should == "NV" }
+        its([:pos])  { should == [:noun, :verb_usu_participle] }
+      end
+
+      describe "ulcerously" do
+        subject { pos.find("ulcerously") }
+        its([:word]) { should == "ulcerously" }
+        its([:code]) { should == "v" }
+        its([:pos])  { should == [:adverb] }
+      end
+    end
   end
 end
