ff23c0d2df613bbfb067b507d63da3641c5088a7
Dan Richert <dan.richert@gmail.com>
2012-01-25 20:21:09 -0500

    Don't autoload

--- lib/moby.rb
+++ lib/moby.rb
@@ -3,7 +3,7 @@ $:.unshift(File.dirname(__FILE__))
 module Moby
   VERSION = "0.0.1"
 
-  autoload :PartsOfSpeech, "moby/parts_of_speech.rb"
+  require "moby/parts_of_speech"
 
   def self.base_path
     File.expand_path("../..", __FILE__)
