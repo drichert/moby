61b152a968a05c696c37ef7063c9fb6214f63d9c
Dan Richert <dan.richert@gmail.com>
2012-01-31 19:30:45 -0500

    move VERSION to moby/version

--- lib/moby.rb
+++ lib/moby.rb
@@ -1,10 +1,9 @@
 $:.unshift(File.dirname(__FILE__))
 
+require 'moby/version'
+require "moby/parts_of_speech"
+
 module Moby
-  VERSION = "0.0.1"
-
-  require "moby/parts_of_speech"
-
   def self.base_path
     File.expand_path("../..", __FILE__)
   end
