f7927151d51f4fd529ef8dadc714e1ea4f5fc355
Dan Richert <dan.richert@gmail.com>
2012-02-07 08:39:44 -0500

    Add spec placeholders

---
+++ spec/moby/words_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+module Moby
+  describe Words do
+    describe "#acronyms" do
+      pending
+    end
+
+    describe "#common" do
+      pending
+    end
+
+    describe "#compound" do
+      pending
+    end
+
+    describe "#crossword" do
+      pending
+    end
+
+    describe "#crossword_additions" do
+      pending
+    end
+
+    describe "#fiction" do
+      pending
+    end
+
+    describe "#freq" do
+      pending
+    end
+
+    describe "#freq_usenet_92" do
+      pending
+    end
+
+    describe "#kjvfreq" do
+      pending
+    end
+
+    describe "#names" do
+      pending
+    end
+
+    describe "#female_names" do
+      pending
+    end
+
+    describe "#male_names" do
+      pending
+    end
+
+    describe "#misspelled" do
+      pending
+    end
+
+    describe "#places" do
+      pending
+    end
+
+    describe "#single" do
+      pending
+    end
+
+    describe "#us_constitution" do
+      pending
+    end
+  end
+end
